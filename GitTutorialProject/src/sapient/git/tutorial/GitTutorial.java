ebf7193edc42f4d372301bb80f7878133243e310
Madhav Arora <maddyarora@yahoo.com>
2016-01-10 15:26:31 +0530

    change 777

--- GitTutorialProject/src/sapient/git/tutorial/GitTutorial.java
+++ GitTutorialProject/src/sapient/git/tutorial/GitTutorial.java
@@ -6,7 +6,7 @@ public class GitTutorial {
 	public static void main(String[] args){
 		
 		
-		System.out.println("Hello Git new change 77 ");
+		System.out.println("Hello Git new change 777 ");
 	}
 
 }
